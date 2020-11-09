d38f24a4c6051d40186e148d2cdd26a312e649df
Miguelangel Diaz <spalduing12@gmail.com>
2020-11-09 14:21:59 -0500

    a

--- myscripts.js
+++ myscripts.js
@@ -92,7 +92,7 @@ function removeItem(e) {
       let liTag = e.target.parentElement.parentElement;
       let ulTag = document.getElementById("notes");
       ulTag.removeChild(liTag);
-      //count -= 1;
+      count -= 1;
       window.localStorage.setItem("count", count);
       window.localStorage.removeItem(e.currentTarget.parentElement.id);
     }
